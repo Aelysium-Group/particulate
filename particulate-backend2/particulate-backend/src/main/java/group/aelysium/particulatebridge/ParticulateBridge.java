039f0df893d30a327b9b4f65138887700d46c32b
Nathan Martin <nathan@noknt.net>
2023-06-27 01:34:51 -0400

    Switch backend to Java

---
+++ particulate-backend2/particulate-backend/src/main/java/group/aelysium/particulatebridge/ParticulateBridge.java
@@ -0,0 +1,12 @@
+package group.aelysium;
+
+/**
+ * Bridge the gap between the particulate control dashboard and Redis!
+ */
+public class ParticulateBridge
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ particulate-backend2/particulate-backend/src/main/java/group/aelysium/particulatebridge/lib/model/IKLifecycle.java
@@ -0,0 +1,21 @@
+package group.aelysium.lib.model;
+
+import java.util.Map;
+
+public abstract class IKLifecycle extends Serviceable {
+    protected IKLifecycle(Map<Class<? extends Service>, Service> services) {
+        super(services);
+    }
+
+    /**
+     * Kill the object. Once this has been called the object should be guaranteed to be garbage collected.
+     */
+    protected abstract void kill();
+
+    /**
+     * Init the object.
+     */
+    protected static IKLifecycle init() {
+        return null;
+    }
+}
